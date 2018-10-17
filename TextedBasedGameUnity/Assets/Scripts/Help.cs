66c483ec569ea35246f0ed76fba8272b5a0a336c
SwordOfWrynn <bubba.wiens@gmail.com>
2018-10-17 07:59:34 -0700

    Foo

---
+++ TextedBasedGameUnity/Assets/Scripts/Help.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TextAdventure/InputActions/Help")]
+public class Help : InputAction {
+
+    public override void RepsondToInput(GameController controller, string[] separatedInputWords)
+    {
+        controller.LogStringWithReturn("These are the commands you can use:");
+
+        for (int i = 0; i < controller.inputActions.Length; i++)
+        {
+            if(controller.inputActions[i].keyWord != "help")
+                controller.LogStringWithReturn("\n" + controller.inputActions[i].keyWord);
+        }
+    }
+
+}

4739e5aefb684d361474d829d641f43c944cc535
SwordOfWrynn <bubba.wiens@gmail.com>
2018-10-18 09:19:33 -0700

    You can log history to text file

--- TextedBasedGameUnity/Assets/Scripts/GameController.cs
+++ TextedBasedGameUnity/Assets/Scripts/GameController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -108,4 +110,33 @@ public class GameController : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void LogToTextFile(string[] separatedInputWords)
+    {
+        string path = separatedInputWords[1]+ ".txt";
+
+        // This text is added only once to the file.
+        //if (!File.Exists(path))
+        //{
+        // Create a file to write to.
+        //     string createText = displayText.text;
+        //     File.WriteAllText(path, createText);
+        string[] actionLogArray = actionLog.ToArray();
+
+        string[] createText = (actionLogArray);
+        File.WriteAllLines(path, createText);
+        //}
+
+        // This text is always added, making the file longer over time
+        // if it is not deleted.
+        /*string appendText = "This is extra text" + Environment.NewLine;
+        File.AppendAllText(path, appendText);
+
+        // Open the file to read from.
+        string readText = File.ReadAllText(path);
+        Debug.Log(readText);*/
+
+        LogStringWithReturn("Game history logged to " + path);
+    }
+
 }
---
+++ TextedBasedGameUnity/Assets/Scripts/Log.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TextAdventure/InputActions/Log")]
+public class Log : InputAction {
+
+    public override void RepsondToInput(GameController controller, string[] separatedInputWords)
+    {
+        controller.LogToTextFile(separatedInputWords);
+    }
+
+}

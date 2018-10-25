12b25cc3085f281cae70498a4ce69a8399794135
Nathan Weis <17.nathan.weis@ksd.org>
2018-10-25 08:51:06 -0700

    Rooms and items and responses

---
+++ TextedBasedGameUnity/Assets/Scripts/ItemResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TextAdventure/ActionResponses/ItemResponse")]
+public class ItemResponse : ActionResponse {
+
+    public InteractableObject itemToGive;
+    [TextArea]
+    public string successTextResponse;
+    [TextArea]
+    public string failureTextResponse;
+
+    public override bool DoActionResponse(GameController controller)
+    {
+        if (!controller.interactableItems.nounsInInventory.Contains(itemToGive.noun))
+        {
+            controller.interactableItems.nounsInInventory.Add(itemToGive.noun);
+            controller.LogStringWithReturn(successTextResponse);
+        }
+        else
+        {
+            controller.LogStringWithReturn(failureTextResponse);
+        }
+
+        return true;
+    }
+}

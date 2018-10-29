ef32c66a3063861c3b6f568499ff666b7b78d4d3
SwordOfWrynn <bubba.wiens@gmail.com>
2018-10-29 09:34:07 -0700

    Foo

--- TextedBasedGameUnity/Assets/Scripts/Take.cs
+++ TextedBasedGameUnity/Assets/Scripts/Take.cs
@@ -7,12 +7,17 @@ public class Take : InputAction {
 
     public override void RepsondToInput(GameController controller, string[] separatedInputWords)
     {
-        Dictionary<string, string> takeDictionary = controller.interactableItems.Take(separatedInputWords);
-
-        if (takeDictionary != null)
+        if (separatedInputWords[1] != "nap")
         {
-            controller.LogStringWithReturn(controller.TestVerbDictionaryWithNoun(takeDictionary, separatedInputWords[0], separatedInputWords[1]));
+            Dictionary<string, string> takeDictionary = controller.interactableItems.Take(separatedInputWords);
+
+            if (takeDictionary != null)
+            {
+                controller.LogStringWithReturn(controller.TestVerbDictionaryWithNoun(takeDictionary, separatedInputWords[0], separatedInputWords[1]));
+            }
         }
+        else
+            controller.LogStringWithReturn("You take a quick nap");
     }
 
 }

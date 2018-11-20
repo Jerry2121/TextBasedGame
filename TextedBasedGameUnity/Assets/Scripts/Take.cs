9bed3b9ae1fe2b1112926d0b97b590c6a24f3385
SwordOfWrynn <bubba.wiens@gmail.com>
2018-11-20 08:07:53 -0800

    Reordered the take input action code

--- TextedBasedGameUnity/Assets/Scripts/Take.cs
+++ TextedBasedGameUnity/Assets/Scripts/Take.cs
@@ -7,17 +7,19 @@ public class Take : InputAction {
 
     public override void RepsondToInput(GameController controller, string[] separatedInputWords)
     {
-        if (separatedInputWords[1] != "nap")
+        if (separatedInputWords[separatedInputWords.Length - 1] == "nap" && separatedInputWords.Length == 2)
+        {
+            controller.LogStringWithReturn("You take a quick nap!\n...\nRefreshing!");
+        }
+        else
         {
             Dictionary<string, string> takeDictionary = controller.interactableItems.Take(separatedInputWords);
 
             if (takeDictionary != null)
             {
                 controller.LogStringWithReturn(controller.TestVerbDictionaryWithNoun(takeDictionary, separatedInputWords[0], separatedInputWords[1]));
+
             }
         }
-        else
-            controller.LogStringWithReturn("You take a quick nap!\n...\nRefreshing!");
     }
-
 }
